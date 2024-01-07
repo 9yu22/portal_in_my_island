18697105e21ad5a4923ca985ab3d39bb72954d12
Daeun-Oh <annsomde@naver.com>
2024-01-08 02:46:56 +0900

    게임모드, 캐릭터 Move C++ 클래스 수정 및 컨트롤러 클래스 추가
    새로 만든 컨트롤러로 움직여야 하는데, 기본으로 들어가 있는 캐릭터 클래스로 움직임.

---
+++ Source/blockers/BKPlayerController.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BKPlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/Pawn.h"
+
+void ABKPlayerController::BeginPlay()
+{
+	// 원본의 BeginPlay
+	// 부모
+	Super::BeginPlay();
+
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(MappingContext, 0);
+	}
+}
+
+void ABKPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABKPlayerController::InputMove);
+	}
+}
+
+void ABKPlayerController::InputMove(const FInputActionValue& value)
+{
+	FVector2D inputAxis = value.Get<FVector2D>();
+
+	if (ACharacter* controlledCharacter = GetCharacter())
+	{
+		controlledCharacter->GetMovementComponent()->AddInputVector(FVector(inputAxis.Y, inputAxis.X, 0));
+	}
+}
---
+++ Source/blockers/BKPlayerController.h
@@ -0,0 +1,34 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BKPlayerController.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class BLOCKERS_API ABKPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+	//     읽기 전용           쓰기 전용            읽기 쓰기
+	// BlueprintReadOnly / BlueprintWriteOnly / BlueprintReadWrite
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputMappingContext* MappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* MoveAction;
+
+protected:
+	// 게임이 시작하는 BeginPlay (컨트롤러가 첫 번째 업데이트를 하기 전에 호출)
+	virtual void BeginPlay() override;
+	// 입력을 준비하는 함수
+	virtual void SetupInputComponent() override;
+
+	// 움직이라는 명령을 받음
+	UFUNCTION()
+	virtual void InputMove(const FInputActionValue& value);
+};
