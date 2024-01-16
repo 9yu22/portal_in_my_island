b536fdcfbb9f9f345a4b4a2f669722b21cb6b9d1
Daeun-Oh <annsomde@naver.com>
2024-01-16 22:40:53 +0900

    플레이어 컨트롤러 이동 구현
    임시 캐릭터로 이동 구현함

--- Source/blockers/blockersGameMode.cpp
+++ Source/blockers/blockersGameMode.cpp
@@ -11,7 +11,7 @@ AblockersGameMode::AblockersGameMode()
 	PlayerControllerClass = ABKPlayerController::StaticClass();
 
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blockers/Blueprints/BP_Player"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
