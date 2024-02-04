f789e97593e17f1e0daeceeebbee86ebdee3482e
givoontake <givoontake@gmail.com>
2024-02-05 02:34:16 +0900

    초기 서버 코드

---
+++ DemoServer/DemoServer.h
@@ -0,0 +1,38 @@
+#pragma once
+#define WOOD 1
+#define ROCK 2
+#define IRON 3
+
+#define NONE 11
+#define ADD_BLOCK 12
+#define REMOVE_BLOCK 13
+
+#define BUFFERSIZE 1024
+#define SERVERPORT 8000
+
+struct packetHeader {
+	char packetSize; 
+	char packetType;
+};
+
+struct blockInfo {
+	char packetSize;
+	char packetType;
+	float x, y, z;
+	char blockType;
+};
+
+struct LocationInfo {
+	char packetSize;
+	char packetType;
+	float posX;
+	float posY;
+	float posZ;
+};
+
+void InitBlockList();
+void SendBlockList(SOCKET client_sock);
+void RecvPacket(SOCKET client_sock);
+void AddGameBlock(const blockInfo block);
+void RemoveGameBlock(const blockInfo block);
+//void SendGameBlocks(SOCKET client_sock);
