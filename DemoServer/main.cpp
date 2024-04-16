df113462a7aeac2c18737f30c6f6f77842e8b6ff
Kang Eun Hyeok <givoontake@gmail.com>
2024-04-16 19:44:04 +0900

    서버 IOCP Single Thread로 변경

--- DemoServer/main.cpp
+++ DemoServer/main.cpp
@@ -1,7 +1,6 @@
 ﻿// DemoServer.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 
 #include "Common.h"
-#include "gameServer.h"
 #include "DemoServer.h"
 #include "player.h"
 
---
+++ DemoServer/protocol.h
@@ -0,0 +1,74 @@
+constexpr int PORT_NUM = 4040;
+constexpr int BUF_SIZE = 256; 
+constexpr int NAME_SIZE = 20;
+
+constexpr char TEST = 100;
+constexpr char LOCATION = 90;
+
+constexpr char CS_LOGIN = 0;
+constexpr char CS_MOVE = 1;
+
+constexpr char SC_LOGIN_INFO = 2;
+constexpr char SC_ADD_PLAYER = 3;
+constexpr char SC_REMOVE_PLAYER = 4;
+constexpr char SC_MOVE_PLAYER = 5;
+
+#pragma pack (push, 1)
+
+//struct UPacketHeader {
+//    int8 size;
+//    int8 type;
+//};
+
+struct CS_LOGIN_PACKET {
+    u_char size;
+    char type;
+    //char name[NAME_SIZE];
+};
+
+struct CS_MOVE_PACKET {
+    u_char size;
+    char type;
+    short x, y, z;
+};
+
+struct SC_LOGIN_INFO_PACKET {
+    u_char size;
+    char type;
+    char id;
+    float x, y, z;
+};
+
+struct SC_ADD_PLAYER_PACKET {
+    u_char size;
+    char type;
+    char id;
+    float x, y, z;
+    //char	name[NAME_SIZE];
+};
+
+struct SC_REMOVE_PLAYER_PACKET {
+    u_char size;
+    char type;
+    short id;
+};
+
+struct SC_MOVE_PLAYER_PACKET {
+    u_char size;
+    char type;
+    char id;
+    float x, y, z;
+};
+
+//struct LocationPacket {
+//    uint8 size;
+//    int8 type;
+//    float x, y, z;
+//};
+
+struct TestPacket {
+    u_char size;
+    char type;
+    short number;
+};
+#pragma pack (pop)
