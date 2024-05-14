243581b673a62a2d4e78168d7809b0d927a943d4
9yu22 <ehtlfk2@naver.com>
2024-05-14 23:39:40 +0900

    widget 주석처리

--- Source/blockers/Private/HealthBarWidget.cpp
+++ Source/blockers/Private/HealthBarWidget.cpp
@@ -8,13 +8,13 @@
 void UHealthBarWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
 {
 	
-	Super::NativeTick(MyGeometry, InDeltaTime);
-	
+	//Super::NativeTick(MyGeometry, InDeltaTime);
+	//
 
-	HealthBar->SetPercent(OwnerCharacter->GetHealth() / OwnerCharacter->GetMaxHealth());
+	//HealthBar->SetPercent(OwnerCharacter->GetHealth() / OwnerCharacter->GetMaxHealth());
 
-	FNumberFormattingOptions opts;
-	opts.SetMaximumFractionalDigits(0);
-	CurrentHealthLabel->SetText(FText::AsNumber(OwnerCharacter->GetHealth(), &opts));
-	MaxHealthLabel->SetText(FText::AsNumber(OwnerCharacter->GetMaxHealth(), &opts));
+	//FNumberFormattingOptions opts;
+	//opts.SetMaximumFractionalDigits(0);
+	//CurrentHealthLabel->SetText(FText::AsNumber(OwnerCharacter->GetHealth(), &opts));
+	//MaxHealthLabel->SetText(FText::AsNumber(OwnerCharacter->GetMaxHealth(), &opts));
 }
