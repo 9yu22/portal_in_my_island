b3e5c1d8318a85f9f188cf5904ad28ec42c1ebd8
9yu22 <ehtlfk2@naver.com>
2024-05-19 18:16:57 +0900

    포탈 hp를 띄움. 플레이어가 포탈을 지나면 포탈 hp는 10씩 감소함. 10번 지나면 게임종료됨.

---
+++ Source/blockers/Private/Portal.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Portal.h"
+
+// Sets default values
+APortal::APortal()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	Portalhealth = 100.f;
+	
+	// Create and initialize Sphere Component
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	RootComponent = SphereComponent;
+
+	// Set Sphere Radius and Collision Profile
+	SphereComponent->InitSphereRadius(50.0f);
+	SphereComponent->SetCollisionProfileName(TEXT("Trigger"));
+}
+
+// Called when the game starts or when spawned
+void APortal::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	GetSphereComponent()->OnComponentBeginOverlap.AddDynamic(this, &APortal::OnBeginOverLap);
+	if (PortalHpWidgetClass != nullptr)
+	{
+		PortalHpWidget = CreateWidget(GetWorld(), PortalHpWidgetClass);
+		PortalHpWidget->AddToViewport();
+	}
+}
+
+// Called every frame
+void APortal::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void APortal::OnBeginOverLap(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromsweep, const FHitResult& SweepResult)
+{
+	if (OtherActor->ActorHasTag("Test")) //이게 캐릭터여야함
+	{
+		Portalhealth -= 10.f;
+		if (Portalhealth < 1) {
+			UGameplayStatics::OpenLevel(this, "End");
+		}
+	}
+}
+
---
+++ Source/blockers/Public/Portal.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GameFramework/Actor.h"
+#include "Portal.generated.h"
+
+UCLASS()
+class BLOCKERS_API APortal : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APortal();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	float GetHealth() const { return Portalhealth; }
+	void SetHealth(float val) { Portalhealth = val; }
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Portalhealth;
+
+	UPROPERTY(EditAnywhere, Category = "UI")
+	TSubclassOf<UUserWidget> PortalHpWidgetClass;
+	UUserWidget* PortalHpWidget;
+
+	UFUNCTION()
+	void OnBeginOverLap(class UPrimitiveComponent* HitComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex, bool bFromsweep, const FHitResult& SweepResult);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Collision", meta = (AllowPrivateAccess = "true"))
+	class USphereComponent* SphereComponent;
+	USphereComponent* GetSphereComponent() const { return SphereComponent; }
+
+};
