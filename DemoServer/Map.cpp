d9dbeb7ca77c26807bee605e575367dfb7cfa433
Kang Eun Hyeok <givoontake@gmail.com>
2024-07-09 10:13:56 +0900

    맵을 2차원으로 변경
    1차원으로 계산했던 부분은 부정확해서 2차원으로 변경

--- DemoServer/Map.cpp
+++ DemoServer/Map.cpp
@@ -3,7 +3,7 @@
 
 Map::Map()
 {
-    block_map.resize(MAX_ARRAY_SIZE);
+    block_map.resize(BIG_CHUNK_SIZE, std::vector<Block>(SMALL_CHUNK_SIZE));
     ReadMapData();
 }
 
@@ -13,10 +13,10 @@ Map::~Map()
 
 bool Map::AddBlockToMap(CS_ADD_BLOCK_PACKET* packet)
 {
-    if (packet->blocktype != static_cast<int>(BKEBlock::Air)) {
-        int index = calculateIndex(packet->chunk_index, packet->ix, packet->iy, packet->iz);
-        if (block_map[index].blockType == static_cast<int>(BKEBlock::Air)) {
-            block_map[index].blockType = packet->blocktype;
+    if (packet->blocktype != static_cast<int>(BKEBlock::Air) && packet->chunk_index >= 0) {
+        int index = calculateIndex(packet->ix, packet->iy, packet->iz);
+        if (block_map[packet->chunk_index][index].blockType == static_cast<int>(BKEBlock::Air)) {
+            block_map[packet->chunk_index][index].blockType = packet->blocktype;
             return true;
         }
         return false;
@@ -26,11 +26,11 @@ bool Map::AddBlockToMap(CS_ADD_BLOCK_PACKET* packet)
 
 bool Map::RemoveBlockToMap(CS_REMOVE_BLOCK_PACKET* packet)
 {
-    if (packet->blocktype == static_cast<int>(BKEBlock::Air)) {
-        int index = calculateIndex(packet->chunk_index, packet->ix, packet->iy, packet->iz);
-        if (block_map[index].blockType != static_cast<int>(BKEBlock::Air)) {
-            block_map[index].blockType = packet->blocktype;
-            std::cout << "x: " << packet->ix << " y: " << packet->iy << " z: " << packet->iz << "위치에 블록 추가" << std::endl;
+    if (packet->blocktype == static_cast<int>(BKEBlock::Air) && packet->chunk_index >= 0) {
+        int index = calculateIndex(packet->ix, packet->iy, packet->iz);
+        if (block_map[index][packet->chunk_index].blockType != static_cast<int>(BKEBlock::Air)) {
+            block_map[index][packet->chunk_index].blockType = packet->blocktype;
+            //std::cout << "x: " << packet->ix << " y: " << packet->iy << " z: " << packet->iz << "위치에 블록 추가" << std::endl;
             return true;
         }
         return false;
@@ -53,33 +53,28 @@ void Map::ReadMapData()
     int crossIndex = 0;
 
     while (file >> chunk_index >> x >> y >> z >> blockType) {
-        int index = calculateIndex(chunk_index, x, y, z);
+        int index = calculateIndex(x, y, z);
 
         Block temp;
         temp.blockType = blockType;
 
-        if (block_map[index].blockType == static_cast<int>(BKEBlock::Air)) {
-            block_map[index] = temp;
+        if (block_map[chunk_index][index].blockType == static_cast<int>(BKEBlock::Air)) {
+            block_map[chunk_index][index] = temp;
             blockCount++;
         }
         else
-           // std::cout << "중복 생성된 인덱스 " << "chunkindex: " << chunk_index << "x: " << x << "y: " << y << "z: " << z << std::endl;
             crossIndex++;
     }
-    //std::cout << "생성된 초기 맵 블록 수: " << blockCount << std::endl;
-    //std::cout << "중복 생성된 초기 맵 블록 수: " << crossIndex << std::endl; // 중복이 약간 존재하는데 클라에서 반복문을 중복 순회하는게 있나..? 아니면 내가 잘못 한건가..
+    std::cout << "생성된 맵 블록 수: " << blockCount << std::endl;
+    //std::cout << "중복 생성된 맵 블록 수: " << crossIndex << std::endl;
     file.close();
 }
 
-int Map::calculateIndex(int chunk_index, int x, int y, int z)
+int Map::calculateIndex(int x, int y, int z)
 {
     int index = (x + (y * SMALL_CHUNK_WIDTH) + (z * SMALL_CHUNK_WIDTH * SMALL_CHUNK_WIDTH));
 
-    if (chunk_index > 0)
-        return chunk_index * index;
-
-    else if (chunk_index == 0)
-        return BIG_CHUNK_SIZE * index;
+    return index;
 }
 
 Block::Block()
